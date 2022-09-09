813101f290bad165f2f55a39cc7f47cc2a80f1a0
Therealneatty <damibusari01@gamil.com>
2022-09-08 22:01:57 -0700

    Task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main-print a text
  *
- * Return: Always 1
+ * Return: Always 1(successful)
  */
 int main(void)
 {
-	write(2,"and that bpiece of art is useful\"-Dora Korpar,2015-10-19\n",59);
+	write(2,"and that bpiece of art is useful\" -Dora Korpar,2015-10-19\n",59);
 	return(1);
 }
