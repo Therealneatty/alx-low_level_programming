d8eadc20405be25bc59a71590237da6384996f0f
Therealneatty <damibusari01@gamil.com>
2022-09-08 21:16:01 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Print
- *
- * Return: Always 0 (success)
+ * main-entry point
+ * Description:print a string using followed by a new line
+ * Return: 0
  */
-{
-	puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
+ 
