89a968ce288be7ef53dd67cbdd65bb70e740f956
Therealneatty <damibusari01@gamil.com>
2022-09-09 16:16:39 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,14 +7,15 @@
  * Return: 0
  */
 int main(void)
-{char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-if (ch != 'e' && ch != 'q')
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
 {
 	putchar(ch);
 }
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	 putchar(ch);
 }
 putchar('\n');
 return (0);
-}
